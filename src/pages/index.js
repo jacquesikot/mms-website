6f05fa6f02711b391d08fac8f75fe3cf1f9c66a5
Jacques Mac <mac@Jacques-MacBook-Pro.local>
2021-07-16 19:11:19 +0100

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { ThemeProvider } from 'theme-ui';
+import { StickyProvider } from 'contexts/app/app.provider';
+import { VideoProvider } from 'contexts/video/video.provider';
+import theme from 'theme';
+import SEO from 'components/seo';
+import Layout from 'components/layout';
+import Modal from 'components/modal';
+import Banner from 'sections/banner';
+import CtaOne from 'sections/cta-one';
+import FAQ from 'sections/faq';
+import CtaTwo from 'sections/cta-two';
+import WorkFlow from 'sections/workflow';
+import Pricing from 'sections/pricing';
+import PopularCourse from 'sections/popular-course';
+import CtaThree from 'sections/cta-three';
+import FavoriteCourse from 'sections/favorite-course';
+
+export default function IndexPage() {
+  return (
+    <ThemeProvider theme={theme}>
+      <StickyProvider>
+        <VideoProvider>
+          <Layout>
+            <SEO
+              description="Mac Music School Online Academy"
+              title="Mac Music School"
+            />
+            <Banner />
+            <CtaTwo />
+            <CtaThree />
+            <PopularCourse />
+            <WorkFlow />
+            <Pricing />
+            <FAQ />
+            <CtaOne />
+            <Modal />
+          </Layout>
+        </VideoProvider>
+      </StickyProvider>
+    </ThemeProvider>
+  );
+}
---
+++ src/sections/cta-two.js
@@ -0,0 +1,105 @@
+import { Box, Text, Flex, Image, Container } from 'theme-ui';
+import React from 'react';
+import { Link } from 'components/link';
+import BlockTitle from 'components/block-title';
+import ctaTwoImage from 'assets/cta-img-1.png';
+
+const CtaTwo = () => {
+  return (
+    <Box as="section" sx={styles.ctaTwo}>
+      <Container>
+        <Flex sx={styles.ctaTwo.row}>
+          <Box sx={styles.ctaTwo.col}>
+            <Image
+              src={ctaTwoImage}
+              sx={styles.ctaTwo.img}
+              alt="cta two image"
+            />
+          </Box>
+          <Box sx={styles.ctaTwo.col}>
+            <Box sx={styles.ctaTwo.content}>
+              <BlockTitle
+                sx={styles.ctaTwo.blockTitle}
+                tagline="Music school of the future"
+                heading={
+                  'Lorem ipsum dolor \n amet, consectetur \n adipiscing elit'
+                }
+              />
+              <Text as="p" sx={styles.ctaTwo.text}>
+                {
+                  'Get your tests delivered at let home collect sample from the victory of thLorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mi sapien, Lorem ipsum dolor sit amet, consectetur adipiscing.'
+                }
+              </Text>
+              <Link path="#" label="Get Started" variant="buttons.primary" />
+            </Box>
+          </Box>
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
+
+export default CtaTwo;
+
+const styles = {
+  ctaTwo: {
+    paddingBottom: '100px',
+    '@media(min-width: 1200px)': {
+      paddingTop: '100px',
+    },
+    '@media(max-width:1024px)': {
+      paddingBottom: 80,
+    },
+    row: {
+      flexWrap: 'wrap',
+    },
+    col: {
+      flex: '0 0 50%',
+      '@media(max-width:1024px)': {
+        flex: '0 0 100%',
+      },
+    },
+    img: {
+      maxWidth: 'none',
+      position: 'relative',
+      right: '220px',
+      bottom: '45px',
+      '@media(max-width:1024px)': {
+        maxWidth: '100%',
+        right: 'auto',
+        bottom: 'auto',
+      },
+    },
+    content: {
+      paddingLeft: '114px',
+      '@media(max-width:1024px)': {
+        paddingLeft: 0,
+        maxWidth: 475,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        textAlign: 'center',
+      },
+      '@media(max-width:575px)': {
+        maxWidth: '91%',
+      },
+    },
+    blockTitle: {
+      marginBottom: [20, null, null, 30],
+
+      h3: {
+        fontSize: ['24px', null, null, '36px', null, '48px'],
+        lineHeight: 1.15,
+        letterSpacing: ['-0.5px', null, null, '-1.5px'],
+        whiteSpace: ['normal', null, null, null, null, 'pre-line'],
+      },
+    },
+    text: {
+      fontSize: [15, null, null, 18],
+      lineHeight: 2.33,
+      color: 'black',
+      marginBottom: 25,
+      mb: [20, null, null, null, null],
+      whiteSpace: ['normal', null, null, null, 'pre-line'],
+    },
+  },
+};
